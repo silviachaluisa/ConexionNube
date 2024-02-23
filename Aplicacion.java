3351782f0f96b4786f758ad8ffe0f5251001c551
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-23 11:36:32 -0500

    Silvia1

---
+++ Aplicacion.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Aplicacion {
+    private JPanel APP;
+    private JTextField TXF1;
+    private JTextField TXF2;
+    private JTextField TXF3;
+    private JTextField TXF4;
+    private JTextField TXF5;
+    private JTextField TXF6;
+    private JTextField TXF7;
+}
